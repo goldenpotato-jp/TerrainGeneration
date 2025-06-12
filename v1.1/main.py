dd780c7f3b7b2868c1c16e4e1e5f9d3c63c61377
goldenpotato <okina.etsushi@gmail.com>
2025-06-12 19:11:31 +0900

    v1.1追加

---
+++ v1.1/main.py
@@ -0,0 +1,45 @@
+import pyxel
+
+WINDOW_WIDTH = 160
+WINDOW_HEIGHT = 120
+RESOURCE_PATH = "/Users/macbook/goldenpotato/python/TerrainGeneration/v1.0/my_resource.pyxres"
+APP_TITLE = "TerrainGeneration"
+VERSION = "v1.0"
+TERRAIN_SEED = 0
+
+#pyxel edit "/Users/macbook/goldenpotato/python/TerrainGeneration/v1.0/my_resource.pyxres"
+
+class Terrain:
+    def __init__(self, seed):
+        self.seed = seed
+        pyxel.nseed(seed)
+    def get(self, y:int, x:int):
+        height = pyxel.noise(x / 32, y / 32)
+        height += pyxel.noise(x / 32, y / 32) / 2
+        height += pyxel.noise(x / 16, y / 16) / 4
+        height += pyxel.noise(x / 8, y / 8) / 8
+        return height
+
+class App:
+    def __init__(self):
+        pyxel.init(WINDOW_WIDTH, WINDOW_HEIGHT, title = APP_TITLE)
+        pyxel.load(RESOURCE_PATH)
+        self.terrain = Terrain(TERRAIN_SEED)
+        pyxel.run(self.update, self.draw)
+    def update(self):
+        pass
+    def draw(self):
+        pyxel.cls(0)
+        self.draw_map()
+        pyxel.text(140, 110, VERSION, 13)
+    def draw_map(self):
+        for i in range(WINDOW_HEIGHT):
+            for j in range(WINDOW_WIDTH):
+                height = self.terrain.get(i, j)
+                if(height < -0.4):pyxel.pset(j, i, 5)
+                elif(height < -0.2):pyxel.pset(j, i, 12)
+                elif(height < 0):pyxel.pset(j, i, 11)
+                elif(height < 0.6):pyxel.pset(j, i, 3)
+                else:pyxel.pset(j, i, 13)
+
+App()
