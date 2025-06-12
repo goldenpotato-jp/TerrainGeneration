ca0c9fb8a2aec7943374745402018e77e366c1f5
goldenpotato <okina.etsushi@gmail.com>
2025-06-12 20:09:23 +0900

    リファクタリング

--- v1.1/main.py
+++ v1.1/main.py
@@ -16,7 +16,7 @@ class Terrain:
     def __init__(self, seed:int):
         self.seed = seed
         pyxel.nseed(seed)
-    def get(self, y:int, x:int):
+    def get_height(self, y:int, x:int):
         height = pyxel.noise(x / 32, y / 32)
         height += pyxel.noise(x / 32, y / 32) / 2
         height += pyxel.noise(x / 16, y / 16) / 4
@@ -38,10 +38,10 @@ class App:
     def draw_map(self):
         for i in range(WINDOW_HEIGHT):
             for j in range(WINDOW_WIDTH):
-                height = self.terrain.get(i, j)
+                height = self.terrain.get_height(i, j)
                 is_waterfront = False
                 for d in range(4):
-                    if(self.terrain.get(i + dy[d], j + dx[d]) < -0.2):is_waterfront = True
+                    if(self.terrain.get_height(i + dy[d], j + dx[d]) < -0.2):is_waterfront = True
                 if(height < -0.4):pyxel.pset(j, i, 5)
                 elif(height < -0.2):pyxel.pset(j, i, 12)
                 elif(is_waterfront):pyxel.pset(j, i, 15)
